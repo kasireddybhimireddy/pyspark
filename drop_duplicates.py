27fd10c07b94fb2d991ee27ef646e8de4603a7de
kasireddybhimireddy <kasireddybhimireddy@gmail.com>
2024-03-25 10:17:49 +0800

    drop duplicates in spark.sql

---
+++ drop_duplicates.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,max,dense_rank,row_number
+from pyspark.sql.window import Window,WindowSpec
+
+
+spark=SparkSession.builder.appName('')\
+           .getOrCreate()
+
+data=[(1,"s",88),(1,"s",88),(2,"U",80),
+      (4,"e",12),(5,"yy",23),(6,"we",84),
+      (7,"gg",123),(8,"il",23),(5,"yy",23),
+      (10,"ee",11),(11,"kl",20),(12,"ik",88)]
+columns=["emp_id","emp_name","sal"]
+df=spark.createDataFrame(data,columns)
+df.show()
+print(df.count())
+df.createOrReplaceTempView("temp")
+distinct_df=spark.sql(""" select distinct emp_id,emp_name,sal from temp""")
+distinct_df.show()
+print(distinct_df.count())
+df_row_number=spark.sql("""select *,row_number() over (partition by emp_id order by emp_id)as rownumber from temp """)
+df_row_number2=df_row_number.filter(col("rownumber") ==1).drop("rownumber")
+df_row_number2.show()
+print(df_row_number2.count())
+spark.stop()
+
+
